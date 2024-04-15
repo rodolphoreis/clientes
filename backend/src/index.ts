ee9f3700c25496a980a49be01eeb14937079edc2
Rodolpho Reis <rodolphoreis@live.com>
2024-04-15 16:49:40 +0100

    feat: create get

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,3 +2,21 @@ import express from "express";
 import cors from "cors";
 import prismaClient from "../prisma/prismaClient";
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/client", async (_, response) => {
+  try {
+    const read = await prismaClient.cliente.findMany();
+    return response.status(200).json({ message: read, error: false });
+  } catch (error) {
+    return response.status(500).json({ message: error, error: true });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Running server port: ${PORT}`);
+});
